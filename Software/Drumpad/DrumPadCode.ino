9692ab3e19390c0583d1f5cd736f615dfe6aa9e1
Conrad-Rowling <55856224+Conrad-Rowling@users.noreply.github.com>
2020-05-02 17:54:01 -0700

    Update DrumPadCode.ino

--- Software/Drumpad/DrumPadCode.ino
+++ Software/Drumpad/DrumPadCode.ino
@@ -1,10 +1,10 @@
 //int ledPin = P1_0;// LED connected to digital pin 
 //int inPin = 0;
 //int val = 0;       // variable to store the read value
-long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 50;    // the debounce time; increase if the output flickers
-int buttonState;
-int lastButtonState = 0;
+//long lastDebounceTime = 0;  // the last time the output pin was toggled
+//long debounceDelay = 50;    // the debounce time; increase if the output flickers
+//int buttonState;
+//int lastButtonState = 0;
 int flag1_6 = 0, flag1_4 = 0; //etc
 void setup()
 { 
