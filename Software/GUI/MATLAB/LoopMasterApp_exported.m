4826abd3fa16bd87ad404af1573e978dc20b523e
neilkatahira <neilkatahira@gmail.com>
2020-05-22 10:12:15 -0700

    Finalized serial codes for GUI

--- Software/GUI/MATLAB/LoopMasterApp_exported.m
+++ Software/GUI/MATLAB/LoopMasterApp_exported.m
@@ -2,26 +2,29 @@ classdef LoopMasterApp_exported < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure               matlab.ui.Figure
-        stopButton             matlab.ui.control.Button
-        StylesButtonGroup      matlab.ui.container.ButtonGroup
-        Style1Button           matlab.ui.control.ToggleButton
-        Style2Button           matlab.ui.control.ToggleButton
-        Style3Button           matlab.ui.control.ToggleButton
-        Style4Button           matlab.ui.control.ToggleButton
-        Style5Button           matlab.ui.control.ToggleButton
-        Style6Button           matlab.ui.control.ToggleButton
-        BPMSpinnerLabel        matlab.ui.control.Label
-        BPMSpinner             matlab.ui.control.Spinner
-        PlayButton             matlab.ui.control.StateButton
-        RecordSwitchLabel      matlab.ui.control.Label
-        RecordSwitch           matlab.ui.control.Switch
-        LoopMasterLabel        matlab.ui.control.Label
-        MajMinSwitch           matlab.ui.control.RockerSwitch
-        PlayingModeSwitch      matlab.ui.control.RockerSwitch
-        DeleteRecordingButton  matlab.ui.control.Button
-        ExportRecordingButton  matlab.ui.control.Button
-        EEEmerge2020Label      matlab.ui.control.Label
+        UIFigure                 matlab.ui.Figure
+        stopButton               matlab.ui.control.Button
+        StylesButtonGroup        matlab.ui.container.ButtonGroup
+        EDMButton                matlab.ui.control.ToggleButton
+        FunkButton               matlab.ui.control.ToggleButton
+        EasternButton            matlab.ui.control.ToggleButton
+        HouseButton              matlab.ui.control.ToggleButton
+        DrumMachineButton        matlab.ui.control.ToggleButton
+        HipHopButton             matlab.ui.control.ToggleButton
+        RnBButton                matlab.ui.control.ToggleButton
+        ChillButton              matlab.ui.control.ToggleButton
+        PlayButton               matlab.ui.control.StateButton
+        LoopMasterLabel          matlab.ui.control.Label
+        LoopsSwitchLabel         matlab.ui.control.Label
+        SoundEffectsSwitchLabel  matlab.ui.control.Label
+        SoundEffectsSwitch       matlab.ui.control.ToggleSwitch
+        RecordingSwitchLabel     matlab.ui.control.Label
+        RecordingSwitch          matlab.ui.control.ToggleSwitch
+        LoopsSwitch              matlab.ui.control.ToggleSwitch
+        DeleteRecordingButton    matlab.ui.control.Button
+        EEEmerge2020Label        matlab.ui.control.Label
+        ClearLoopsButton         matlab.ui.control.Button
+        ClearSoundEffectsButton  matlab.ui.control.Button
     end
 
     
@@ -46,52 +49,32 @@ classdef LoopMasterApp_exported < matlab.apps.AppBase
             fopen(app.a);
         end
 
-        % Button pushed function: stopButton
-        function stopButtonPushed(app, event)
-            % Add stop functionality here
-            fprintf(app.a, 'z');
-        end
-
         % Selection changed function: StylesButtonGroup
         function StylesButtonGroupSelectionChanged(app, event)
             selectedButton = app.StylesButtonGroup.SelectedObject.Text;
             switch selectedButton
-                case 'Style 1'
+                case 'EDM'
                     fprintf(app.a, 'a');
-                case 'Style 2'
+                case 'Funk'
                     fprintf(app.a, 'b');
-                case 'Style 3'
+                case 'Eastern'
                     fprintf(app.a, 'c');
-                case 'Style 4'
+                case 'House'
                     fprintf(app.a, 'd');
-                case 'Style 5'
+                case 'Drum Machine'
                     fprintf(app.a, 'e');
-                case 'Style 6'
-                    fprintf(app.a, 'f'); 
+                case 'Hip Hop'
+                    fprintf(app.a, 'f');
+                case 'RnB'
+                    fprintf(app.a, 'g'); 
+                case 'Chill'
+                    fprintf(app.a, 'h'); 
             end
         end
 
-        % Value changed function: BPMSpinner
-        function BPMSpinnerValueChanged(app, event)
-            value = app.BPMSpinner.Value + 88;
-            fprintf(app.a, value);
-        end
-
-        % Value changed function: PlayButton
-        function PlayButtonValueChanged(app, event)
-            value = app.PlayButton.Value;
-            if value
-                app.PlayButton.Text = 'Pause';
-                fprintf(app.a, 'g');
-            else
-                app.PlayButton.Text = 'Play';
-                fprintf(app.a, 'h');
-            end
-        end
-
-        % Value changed function: RecordSwitch
-        function RecordSwitchValueChanged(app, event)
-            value = app.RecordSwitch.Value;
+        % Value changed function: LoopsSwitch
+        function LoopsSwitchValueChanged(app, event)
+            value = app.LoopsSwitch.Value;
             if strcmpi(value, 'On')
                 fprintf(app.a, 'i');
             elseif strcmpi(value, 'Off')
@@ -99,34 +82,57 @@ classdef LoopMasterApp_exported < matlab.apps.AppBase
             end
         end
 
-        % Value changed function: MajMinSwitch
-        function MajMinSwitchValueChanged(app, event)
-            value = app.MajMinSwitch.Value;
-            if strcmpi(value, 'Minor')
-                fprintf(app.a, 'k');
-            elseif strcmpi(value, 'Major')
-                fprintf(app.a, 'l');
-            end
+        % Button pushed function: ClearLoopsButton
+        function ClearLoopsButtonPushed(app, event)
+            fprintf(app.a, 'o');
         end
 
-        % Value changed function: PlayingModeSwitch
-        function PlayingModeSwitchValueChanged(app, event)
-            value = app.PlayingModeSwitch.Value;
-            if strcmpi(value, 'Performance')
-                fprintf(app.a, 'm');
-            elseif strcmpi(value, 'Practice')
-                fprintf(app.a, 'n');
+        % Value changed function: RecordingSwitch
+        function RecordingSwitchValueChanged(app, event)
+            value = app.RecordingSwitch.Value;
+            if strcmpi(value, 'On')
+                fprintf(app.a, 'k');
+            elseif strcmpi(value, 'Off')
+                fprintf(app.a, 'l');
             end
         end
 
         % Button pushed function: DeleteRecordingButton
         function DeleteRecordingButtonPushed(app, event)
-            fprintf(app.a, 'o');
+            fprintf(app.a, 'p');
         end
 
-        % Button pushed function: ExportRecordingButton
-        function ExportRecordingButtonPushed(app, event)
-            fprintf(app.a, 'p');
+        % Value changed function: SoundEffectsSwitch
+        function SoundEffectsSwitchValueChanged(app, event)
+            value = app.SoundEffectsSwitch.Value;
+            if strcmpi(value, 'On')
+                fprintf(app.a, 'm');
+            elseif strcmpi(value, 'Off')
+                fprintf(app.a, 'n');
+            end
+        end
+
+        % Button pushed function: ClearSoundEffectsButton
+        function ClearSoundEffectsButtonPushed(app, event)
+            fprintf(app.a, 'q');
+        end
+
+        % Value changed function: PlayButton
+        function PlayButtonValueChanged(app, event)
+            value = app.PlayButton.Value;
+            if value % Want to play
+                app.PlayButton.Text = 'Pause';
+                fprintf(app.a, 'x');
+            else % Want to pause
+                app.PlayButton.Text = 'Play';
+                fprintf(app.a, 'y');
+            end
+        end
+
+        % Button pushed function: stopButton
+        function stopButtonPushed(app, event)
+            % Add stop functionality here
+            fprintf(app.a, 'z');
         end
 
         % Close request function: UIFigure
@@ -156,7 +162,7 @@ classdef LoopMasterApp_exported < matlab.apps.AppBase
             % Create stopButton
             app.stopButton = uibutton(app.UIFigure, 'push');
             app.stopButton.ButtonPushedFcn = createCallbackFcn(app, @stopButtonPushed, true);
-            app.stopButton.Position = [519 16 100 22];
+            app.stopButton.Position = [44 28 100 22];
             app.stopButton.Text = 'Stop';
 
             % Create StylesButtonGroup
@@ -164,110 +170,118 @@ classdef LoopMasterApp_exported < matlab.apps.AppBase
             app.StylesButtonGroup.SelectionChangedFcn = createCallbackFcn(app, @StylesButtonGroupSelectionChanged, true);
             app.StylesButtonGroup.Title = 'Styles';
             app.StylesButtonGroup.FontWeight = 'bold';
-            app.StylesButtonGroup.Position = [25 300 123 164];
+            app.StylesButtonGroup.Position = [33 156 123 209];
 
-            % Create Style1Button
-            app.Style1Button = uitogglebutton(app.StylesButtonGroup);
-            app.Style1Button.Text = 'Style 1';
-            app.Style1Button.Position = [11 111 100 22];
-            app.Style1Button.Value = true;
+            % Create EDMButton
+            app.EDMButton = uitogglebutton(app.StylesButtonGroup);
+            app.EDMButton.Text = 'EDM';
+            app.EDMButton.Position = [11 156 100 22];
+            app.EDMButton.Value = true;
 
-            % Create Style2Button
-            app.Style2Button = uitogglebutton(app.StylesButtonGroup);
-            app.Style2Button.Text = 'Style 2';
-            app.Style2Button.Position = [11 90 100 22];
+            % Create FunkButton
+            app.FunkButton = uitogglebutton(app.StylesButtonGroup);
+            app.FunkButton.Text = 'Funk';
+            app.FunkButton.Position = [11 135 100 22];
 
-            % Create Style3Button
-            app.Style3Button = uitogglebutton(app.StylesButtonGroup);
-            app.Style3Button.Text = 'Style 3';
-            app.Style3Button.Position = [11 69 100 22];
+            % Create EasternButton
+            app.EasternButton = uitogglebutton(app.StylesButtonGroup);
+            app.EasternButton.Text = 'Eastern';
+            app.EasternButton.Position = [11 114 100 22];
 
-            % Create Style4Button
-            app.Style4Button = uitogglebutton(app.StylesButtonGroup);
-            app.Style4Button.Text = 'Style 4';
-            app.Style4Button.Position = [11 48 100 22];
+            % Create HouseButton
+            app.HouseButton = uitogglebutton(app.StylesButtonGroup);
+            app.HouseButton.Text = 'House';
+            app.HouseButton.Position = [11 93 100 22];
 
-            % Create Style5Button
-            app.Style5Button = uitogglebutton(app.StylesButtonGroup);
-            app.Style5Button.Text = 'Style 5';
-            app.Style5Button.Position = [11 27 100 22];
+            % Create DrumMachineButton
+            app.DrumMachineButton = uitogglebutton(app.StylesButtonGroup);
+            app.DrumMachineButton.Text = 'Drum Machine';
+            app.DrumMachineButton.Position = [12 72 100 22];
 
-            % Create Style6Button
-            app.Style6Button = uitogglebutton(app.StylesButtonGroup);
-            app.Style6Button.Text = 'Style 6';
-            app.Style6Button.Position = [11 6 100 22];
+            % Create HipHopButton
+            app.HipHopButton = uitogglebutton(app.StylesButtonGroup);
+            app.HipHopButton.Text = 'Hip Hop';
+            app.HipHopButton.Position = [11 51 100 22];
 
-            % Create BPMSpinnerLabel
-            app.BPMSpinnerLabel = uilabel(app.UIFigure);
-            app.BPMSpinnerLabel.HorizontalAlignment = 'right';
-            app.BPMSpinnerLabel.Position = [291 189 32 22];
-            app.BPMSpinnerLabel.Text = 'BPM';
+            % Create RnBButton
+            app.RnBButton = uitogglebutton(app.StylesButtonGroup);
+            app.RnBButton.Text = 'RnB';
+            app.RnBButton.Position = [11 30 100 22];
 
-            % Create BPMSpinner
-            app.BPMSpinner = uispinner(app.UIFigure);
-            app.BPMSpinner.Limits = [40 160];
-            app.BPMSpinner.RoundFractionalValues = 'on';
-            app.BPMSpinner.ValueDisplayFormat = '%.0f';
-            app.BPMSpinner.ValueChangedFcn = createCallbackFcn(app, @BPMSpinnerValueChanged, true);
-            app.BPMSpinner.Position = [338 189 100 22];
-            app.BPMSpinner.Value = 80;
+            % Create ChillButton
+            app.ChillButton = uitogglebutton(app.StylesButtonGroup);
+            app.ChillButton.Text = 'Chill';
+            app.ChillButton.Position = [11 9 100 22];
 
             % Create PlayButton
             app.PlayButton = uibutton(app.UIFigure, 'state');
             app.PlayButton.ValueChangedFcn = createCallbackFcn(app, @PlayButtonValueChanged, true);
             app.PlayButton.Text = 'Play';
-            app.PlayButton.Position = [520 50 100 22];
-
-            % Create RecordSwitchLabel
-            app.RecordSwitchLabel = uilabel(app.UIFigure);
-            app.RecordSwitchLabel.HorizontalAlignment = 'center';
-            app.RecordSwitchLabel.FontWeight = 'bold';
-            app.RecordSwitchLabel.Position = [46.5 111 47 22];
-            app.RecordSwitchLabel.Text = 'Record';
-
-            % Create RecordSwitch
-            app.RecordSwitch = uiswitch(app.UIFigure, 'slider');
-            app.RecordSwitch.ValueChangedFcn = createCallbackFcn(app, @RecordSwitchValueChanged, true);
-            app.RecordSwitch.Position = [47 80 45 20];
+            app.PlayButton.Position = [44 66 100 22];
 
             % Create LoopMasterLabel
             app.LoopMasterLabel = uilabel(app.UIFigure);
             app.LoopMasterLabel.HorizontalAlignment = 'center';
             app.LoopMasterLabel.FontName = 'TI-Nspire';
             app.LoopMasterLabel.FontSize = 20;
-            app.LoopMasterLabel.Position = [302 432 126 29];
+            app.LoopMasterLabel.Position = [255 414 126 29];
             app.LoopMasterLabel.Text = 'LoopMaster';
-
-            % Create MajMinSwitch
-            app.MajMinSwitch = uiswitch(app.UIFigure, 'rocker');
-            app.MajMinSwitch.Items = {'Minor', 'Major'};
-            app.MajMinSwitch.ValueChangedFcn = createCallbackFcn(app, @MajMinSwitchValueChanged, true);
-            app.MajMinSwitch.Position = [135 59 20 45];
-            app.MajMinSwitch.Value = 'Major';
-
-            % Create PlayingModeSwitch
-            app.PlayingModeSwitch = uiswitch(app.UIFigure, 'rocker');
-            app.PlayingModeSwitch.Items = {'Performance', 'Practice'};
-            app.PlayingModeSwitch.ValueChangedFcn = createCallbackFcn(app, @PlayingModeSwitchValueChanged, true);
-            app.PlayingModeSwitch.Position = [201 59 20 45];
-            app.PlayingModeSwitch.Value = 'Practice';
 
             % Create DeleteRecordingButton
             app.DeleteRecordingButton = uibutton(app.UIFigure, 'push');
             app.DeleteRecordingButton.ButtonPushedFcn = createCallbackFcn(app, @DeleteRecordingButtonPushed, true);
-            app.DeleteRecordingButton.Position = [208 306 108 22];
+            app.DeleteRecordingButton.Position = [183 135 108 22];
             app.DeleteRecordingButton.Text = 'Delete Recording';
-
-            % Create ExportRecordingButton
-            app.ExportRecordingButton = uibutton(app.UIFigure, 'push');
-            app.ExportRecordingButton.ButtonPushedFcn = createCallbackFcn(app, @ExportRecordingButtonPushed, true);
-            app.ExportRecordingButton.Position = [400 306 108 22];
-            app.ExportRecordingButton.Text = 'Export Recording';
 
             % Create EEEmerge2020Label
             app.EEEmerge2020Label = uilabel(app.UIFigure);
             app.EEEmerge2020Label.Position = [533 461 101 22];
             app.EEEmerge2020Label.Text = 'EE-Emerge 2020 ';
+
+            % Create LoopsSwitchLabel
+            app.LoopsSwitchLabel = uilabel(app.UIFigure);
+            app.LoopsSwitchLabel.HorizontalAlignment = 'center';
+            app.LoopsSwitchLabel.Position = [351 211 38 22];
+            app.LoopsSwitchLabel.Text = 'Loops';
+
+            % Create LoopsSwitch
+            app.LoopsSwitch = uiswitch(app.UIFigure, 'toggle');
+            app.LoopsSwitch.ValueChangedFcn = createCallbackFcn(app, @LoopsSwitchValueChanged, true);
+            app.LoopsSwitch.Position = [359 269 20 45];
+
+            % Create SoundEffectsSwitchLabel
+            app.SoundEffectsSwitchLabel = uilabel(app.UIFigure);
+            app.SoundEffectsSwitchLabel.HorizontalAlignment = 'center';
+            app.SoundEffectsSwitchLabel.Position = [464 211 80 22];
+            app.SoundEffectsSwitchLabel.Text = 'Sound Effects';
+
+            % Create SoundEffectsSwitch
+            app.SoundEffectsSwitch = uiswitch(app.UIFigure, 'toggle');
+            app.SoundEffectsSwitch.ValueChangedFcn = createCallbackFcn(app, @SoundEffectsSwitchValueChanged, true);
+            app.SoundEffectsSwitch.Position = [493 269 20 45];
+
+            % Create RecordingSwitchLabel
+            app.RecordingSwitchLabel = uilabel(app.UIFigure);
+            app.RecordingSwitchLabel.HorizontalAlignment = 'center';
+            app.RecordingSwitchLabel.Position = [207 211 60 22];
+            app.RecordingSwitchLabel.Text = 'Recording';
+
+            % Create RecordingSwitch
+            app.RecordingSwitch = uiswitch(app.UIFigure, 'toggle');
+            app.RecordingSwitch.ValueChangedFcn = createCallbackFcn(app, @RecordingSwitchValueChanged, true);
+            app.RecordingSwitch.Position = [226 269 20 45];
+
+            % Create ClearLoopsButton
+            app.ClearLoopsButton = uibutton(app.UIFigure, 'push');
+            app.ClearLoopsButton.ButtonPushedFcn = createCallbackFcn(app, @ClearLoopsButtonPushed, true);
+            app.ClearLoopsButton.Position = [320 135 100 22];
+            app.ClearLoopsButton.Text = 'Clear Loops';
+
+            % Create ClearSoundEffectsButton
+            app.ClearSoundEffectsButton = uibutton(app.UIFigure, 'push');
+            app.ClearSoundEffectsButton.ButtonPushedFcn = createCallbackFcn(app, @ClearSoundEffectsButtonPushed, true);
+            app.ClearSoundEffectsButton.Position = [443 135 122 22];
+            app.ClearSoundEffectsButton.Text = 'Clear Sound Effects';
 
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
